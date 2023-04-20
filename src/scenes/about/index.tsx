2318b79ef057466f482fb14482dc37fd7a10a814
aniiaham <aniya_axm@icloud.com>
2023-04-20 15:18:31 -0400

    working on about page

--- src/scenes/about/index.tsx
+++ src/scenes/about/index.tsx
@@ -7,11 +7,11 @@ type Props = {};
 const Works = (props: Props) => {
   return (
     <section
-      className="container relative mx-auto mt-10 h-full w-full max-w-xl bg-white bg-cover pb-20 first-letter:gap-16 md:h-full"
+      className="container relative mx-auto mt-4 h-full w-full max-w-xl bg-white bg-cover pb-20 first-letter:gap-16 md:h-full"
       id="about"
     >
       <motion.div
-        className=" mx-auto"
+        className=" bg-primary-100 mx-auto w-full py-20"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
@@ -21,21 +21,20 @@ const Works = (props: Props) => {
           visible: { opacity: 1, x: 0 },
         }}
       >
-        {/* <img
-          alt="background"
-          src={WavesAboutPage}
-          className="absolute max-h-fit object-cover mix-blend-multiply"
-        /> */}
-        <div>
-          <div className="flex items-center justify-center text-3xl text-gray">
-            <HText>WHO AM I?</HText>
-          </div>
-          <p className="md:text-startv mt-8 font-primary text-lg font-medium">
-            As a Junior Front-End Developer I am a highly motivated and
-            detail-oriented individual with a strong foundation in HTML, CSS,
-            JavaScript, React.js, TypeScript and Git.
-          </p>
+        <div className="flex items-center justify-center text-3xl text-gray">
+          <HText>WHO AM I?</HText>
         </div>
+        <p className="md:text-startv mt-8 font-primary text-lg font-medium">
+          As a Junior Front-End Developer I am a highly motivated and
+          detail-oriented individual with a strong foundation in HTML, CSS,
+          JavaScript, React.js, TypeScript and Git.
+        </p>
+        <div className="mt-20 flex items-center justify-center text-gray">
+          <HText>SKILLS</HText>
+        </div>
+        <p className="md:text-startv mt-8 font-primary text-lg font-medium">
+          HTML/CSS JavaScript TypeScript React.js TailwindCSS Figma
+        </p>
       </motion.div>
     </section>
   );
