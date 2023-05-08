44c630471d086ba600ce34843852001fab1aba36
aniiaham <aniya_axm@icloud.com>
2023-05-08 18:43:34 -0400

    feat: icon animations

--- src/shared/Socials.tsx
+++ src/shared/Socials.tsx
@@ -2,6 +2,7 @@ import LinkedIn from "@/assets/Linkedin.svg";
 import Instagram from "@/assets/instagram.svg";
 import Telegram from "@/assets/telegram.svg";
 import GitHub from "@/assets/github.svg";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -10,34 +11,51 @@ const Socials = (props: Props) => {
     <div>
       <div className="mx-auto w-5/6">
         <div className="flex items-center justify-evenly">
-          <a
+          <motion.a
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
             href="https://www.instagram.com/aniiahamilton/"
             target="_blank"
             className="cursor-pointer"
           >
             <img alt="instagram" src={Instagram} />
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
             href="https://t.me/aniiahamilton"
             target="_blank"
             className="cursor-pointer"
           >
             <img alt="telegram" src={Telegram} />
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
             href="https://github.com/aniiaham"
             target="_blank"
             className="cursor-pointer"
           >
             <img alt="github" src={GitHub} />
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 1 },
+            }}
             href="https://www.linkedin.com/in/aniia-hamilton/"
             target="_blank"
             className="cursor-pointer"
           >
             <img alt="linkedin" src={LinkedIn} />
-          </a>
+          </motion.a>
         </div>
       </div>
     </div>
