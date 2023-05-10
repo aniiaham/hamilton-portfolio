0a5337493d0149ecb5a508e33db6c5ff41df0c1f
aniiaham <aniya_axm@icloud.com>
2023-05-10 19:29:13 -0400

    fix: navbar

--- src/scenes/navBar/index.tsx
+++ src/scenes/navBar/index.tsx
@@ -1,36 +1,36 @@
 import Logo from "@/assets/Logo.svg";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 const NavBar = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 620px)");
   return (
-    <nav>
-      <div className="fixed top-0 z-50 flex w-full items-center justify-between bg-stone py-5">
-        <div className="mx-auto flex items-center justify-between">
-          <div className="flex w-full gap-20">
-            {/*LEFT SIDE*/}
-            <img
-              alt="logo"
-              src={Logo}
-              className="flex items-start justify-start"
-            />
-            {/*RIGHT SIDE*/}
-            <div className="flex w-full items-center justify-between">
-              <div className="flex items-center justify-between gap-16 text-lg">
-                <a href="/" className="font-primary">
-                  HOME
-                </a>
-                <a href="/#about" className="font-primary">
-                  ABOUT
-                </a>
-                <a href="/#works" className="font-primary">
-                  WORKS
-                </a>
-                <a href="/#contact" className="font-primary">
-                  CONTACT
-                </a>
-              </div>
+    <nav className="fixed top-0 z-50 flex w-full items-center justify-between bg-stone py-5">
+      <div className="flex w-full gap-20">
+        {/*LEFT SIDE*/}
+        <img
+          alt="logo"
+          src={Logo}
+          className="flex items-start justify-start pl-6"
+        />
+        {/*RIGHT SIDE*/}
+        {isAboveMediumScreens && (
+          <div className="flex w-full items-center justify-center">
+            <div className="flex items-center gap-16 text-lg">
+              <a href="/" className="font-primary">
+                HOME
+              </a>
+              <a href="/#about" className="font-primary">
+                ABOUT
+              </a>
+              <a href="/#works" className="font-primary">
+                WORKS
+              </a>
+              <a href="/#contact" className="font-primary">
+                CONTACT
+              </a>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </nav>
   );
